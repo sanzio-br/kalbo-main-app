da2e7bd50a6400d75c5328dbb61a155cd2276892
kiariepeter13 <kiariepeter13@gmail.com>
2022-05-11 10:31:16 +0300

    first commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 if (process.env.NODE_ENV === "production") {
     app.use(express.static('build'));
     app.get('*', (req, res) => {
-        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        req.sendfile(path.resolve(__dirname, 'build', 'index.html'));
     })
 }
 
